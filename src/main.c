fb596498499cd1624b28f5904da6b3b0510bf0fc
ETHXN <migethanu@gmail.com>
2026-08-04 23:51:21 +0100

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
     // Flush after every printf
     setbuf(stdout, NULL);
 
     printf("$ ");
+
+    char command[1024];
+    fgets(command, sizeof(command), stdin);
+
+    // remove the trailing newline
+    command[strcspn(command, "\n")] = '\0';
+
+    printf("%s: command not found\n", command);
 
     return 0;
 }
