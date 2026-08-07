868e63732dcb45b7773f835d33a1ed1495f2d17d
ETHXN <migethanu@gmail.com>
2026-08-07 17:38:02 +0100

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,68 +1,77 @@
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
-int get_input(char *args[]);
-void free_inputs(char *args[]);
+#define MAX_ARGS 10
 
-int main(int argc, char *argv[]) {
+int get_input(char *args[], char *buf, size_t buf_size);
+char *get_path(char *command, size_t command_len);
+
+int main(void) {
     // Flush after every printf
     setbuf(stdout, NULL);
+
+    char inputs[1024];
 
     while (1) {
         printf("$ ");
 
-        char *argv[10];
-        argc = get_input(argv);
+        char *tokens[MAX_ARGS];
+        int token_count = get_input(tokens, inputs, sizeof(inputs));
 
-        if (argc < 0) {
+        if (token_count < 0) {
             putchar('\n');
             break; // Any negative return code means stop execution
+        } else if (token_count == 0) {
+            continue;
         }
 
-        if (strcmp(argv[0], "exit") == 0) {
-            free_inputs(argv);
+        if (strcmp(tokens[0], "exit") == 0) {
             break;
-        } else if (strcmp(argv[0], "echo") == 0) {
+        } else if (strcmp(tokens[0], "echo") == 0) {
             /* print the arguments entered after echo */
-            for (int i = 1; i < argc; i++)
-                printf("%s ", argv[i]);
+            for (int i = 1; i < token_count; i++)
+                printf("%s ", tokens[i]);
             putchar('\n');
-        } else {
-            printf("%s: command not found\n", argv[0]);
-        }
+        } else if (strcmp(tokens[0], "type") == 0) {
+            if (strcmp(tokens[1], "type") == 0 ||
+                strcmp(tokens[1], "exit") == 0 ||
+                strcmp(tokens[1], "echo") == 0) {
+                printf("%s is a shell builtin\n", tokens[1]);
+                continue;
+            } else {
+                printf("%s: not found\n", tokens[1]);
+            }
 
-        free_inputs(argv);
+        } else {
+            printf("%s: command not found\n", tokens[0]);
+        }
     }
 
     return 0;
 }
 
-int get_input(char *args[]) {
-    char command[1024];
-    if (fgets(command, sizeof(command), stdin) == NULL) {
-        if (feof(stdin)) {
-            /* Signal EOF to caller (e.g., user pressed Ctrl+D) */
-            return -1;
-        } else if (ferror(stdin)) {
+/* reads input from user into buf and tokenizes it into an array of strings
+ * it returns the number of tokens read a value of -1 indicates error with
+ * stdin or EOF reached
+ */
+int get_input(char *args[], char *buf, size_t buf_size) {
+    if (fgets(buf, buf_size, stdin) == NULL) {
+        if (ferror(stdin))
             perror("fgets failed");
-            return -1;
-        }
+        return -1;
     }
 
     /* remove the trailing newline */
-    command[strcspn(command, "\n")] = '\0';
+    buf[strcspn(buf, "\n")] = '\0';
 
     /* tokenize the input to become arguments */
     int argc = 0;
-    char *token = strtok(command, " ");
+    char *token = strtok(buf, " ");
     while (token != NULL) {
-        args[argc] = strdup(token);
-        /* Error handling for NULL return */
-        if (args[argc] == NULL) {
-            perror("strdup failed");
-            exit(EXIT_FAILURE);
-        }
+        args[argc] = token;
         token = strtok(NULL, " ");
         argc++;
     }
@@ -70,11 +79,4 @@ int get_input(char *args[]) {
     args[argc] = NULL;
 
     return argc;
-}
-
-void free_inputs(char *args[]) {
-    while (*args != NULL) {
-        free(*args);
-        args++;
-    }
 }
